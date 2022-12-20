63f67fc319ed3f78c2eb26d36d15fd087f0d8cce
Osamu Ito <mr.samito@gmail.com>
2022-12-20 15:36:13 -0500

    animation added to Intro image

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,15 @@
+import { motion } from "framer-motion";
+
 const Intro = () => {
+
   return (
     <div className="flex justify-left flex-col text-left pt-[7rem] pb-[7rem] ">
-      <p className="flex-wrap text-6xl leading-snug xl:max-w-[80%] mt-6 mb-6 font-DMSans">
-        <img
+      <p className=" intro flex-wrap text-[3.5em] leading-snug xl:max-w-[80%] mt-6 mb-6 font-DMSans">
+        <motion.img
+          whileHover={{ scale: 2 }}
           src="./assets/face.png"
           alt="face"
-          className="float-left w-[3rem] mr-[1rem]"
+          className="float-left w-[.75em] mr-[.5em]"
         />{" "}
         Hi, I’m Sam. I am a Software Engineer with experience in full stack
         development using JavaScript, React, Ruby/Ruby on Rails, SQL, SQLite,
