779e39bea017698d16fdadeb5000a329f739a051
Osamu Ito <mr.samito@gmail.com>
2022-12-29 20:43:52 -0500

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => {
         </a>
       </div>
       <p className="text-m mt-2 opacity-50 font-DMSans">
-        &copy; {new Date().getFullYear()} Osamu Ito. All rights reserved. This site was built using React & Tailwindcss
+        &copy; {new Date().getFullYear()} Osamu Ito. All rights reserved. <br/> This site was built using React & Tailwindcss
       </p>
     </div>
   );
