3aa2131ff16ecb4be21164b7ac604d84841ffcf0
Osamu Ito <mr.samito@gmail.com>
2023-02-14 14:53:16 -0500

    updating mobile view of resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="flex-col pt-[7rem] pb-[7rem] justify-between text-center items-center font-DMSans">
-      <div className=" items-center grid grid-cols-2 relative ml-[47%] mr-[47%] gap-10">
+      <div className=" items-center grid grid-cols-2 relative ml-[47%] mr-[47%] gap-10 ">
         <a
           href="https://www.linkedin.com/in/osamu-sam-ito/"
           target="_blank"
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -15,7 +15,7 @@ const Resume = () => {
   return (
     <div className="flex pl-[14rem] pr-[10rem] flex-col pt-[7rem] pb-[7rem] flex justify-between items-center">
       <h1 className="text-5xl pb-20">Resume</h1>
-      <iframe
+      {/* <iframe
         id="sam"
         title="resume"
         style={{ border: "1px solid #666CCC", display: "block" }}
@@ -23,7 +23,56 @@ const Resume = () => {
         frameborder="1"
         height="1100"
         width="850"
-      ></iframe>
+      ></iframe> */}
+
+      <div class="outer-pdf">
+        <div class="pdf">
+          <iframe
+            class="desktop-pdf"
+            scrolling="auto"
+            src={resume}
+            width="1100"
+            height="850"
+            type="application/pdf"
+            title="Title"
+          >
+            <p>
+              <em>
+                There is content being displayed here that your browser doesn't
+                support.
+              </em>{" "}
+              <a href="URL HERE" target="_blank">
+                {" "}
+                Please click here to attempt to view the information in a
+                separate browser window.{" "}
+              </a>{" "}
+              Thanks for your patience!
+            </p>
+          </iframe>
+          <iframe
+            class="mobile-pdf"
+            scrolling="auto"
+            src="https://docs.google.com/gview?url=https://docs.google.com/document/d/1qHJ9WRtQq_aU0qOjNMcg9VHSosS9N6llGj976pq8jEk/edit&embedded=true"
+            width="300"
+            height="600"
+            type="application/pdf"
+            title="Title"
+          >
+            <p>
+              <em>
+                There is content being displayed here that your browser doesn't
+                support.
+              </em>{" "}
+              <a href="URL HERE" target="_blank">
+                {" "}
+                Please click here to attempt to view the information in a
+                separate browser window.{" "}
+              </a>{" "}
+              Thanks for your patience!
+            </p>
+          </iframe>
+        </div>
+      </div>
     </div>
   );
 };
