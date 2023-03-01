5c80eb01035268a0d1d5ca2cf5ef5f595a1a98ec
Osamu Ito <mr.samito@gmail.com>
2023-02-28 20:38:10 -0500

    updated image motion

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,14 +6,16 @@ const Intro = () => {
     <div className="flex justify-left flex-col text-left pt-[7rem] pb-[7rem] ">
       <p className=" intro flex-wrap text-[1.75rem] sm:text-[3.5rem] leading-snug xl:max-w-[80%] mt-6 mb-6 font-DMSans">
         <motion.img
+          style={{ cursor: "none"}}
           whileHover={{ scale: 3 }}
+          whileTap={{ scale: 1 }}
           src="./assets/face.png"
           alt="face"
           className="float-left w-[.75em] mr-[.5em]"
         />{" "}
         Hi, I’m Sam! I am a Software Engineer with experience in full stack
-        development using JavaScript, Java, React, Ruby/Ruby on Rails, SQL, SQLite,
-        PostgreSQL.{" "}
+        development using JavaScript, Java, React, Ruby/Ruby on Rails, SQL,
+        SQLite, PostgreSQL.{" "}
       </p>
     </div>
   );
